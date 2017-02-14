f1b14694e9ddd48d8f1c2b8526d7a39695cc830f
Geoff Buesing <gbuesing@gmail.com>
2017-02-14 17:22:28 -0600

    Add sinon stubbing example to tests

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 exports.handler = function(event, context, callback) {
-  var name = event.name || 'World';
-  var greeting = "Hello, " + name;
-  callback(null, {greeting: greeting});
+  var name = event.name || 'World'
+  var greeting = exports.getGreetingPrefix() + name
+  callback(null, {greeting: greeting})
+}
+
+// export so we can stub in tests
+exports.getGreetingPrefix = function() {
+  return "Hello, "
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
-var assert        = require('assert')
-    , lambdaLocal = require('lambda-local')
-    , winston     = require('winston')
+var assert      = require('assert')
+  , lambdaLocal = require('lambda-local')
+  , winston     = require('winston')
+  , sinon       = require('sinon')
+  , helloWorld  = require('../index.js')
 
 
 winston.level = 'warn'
@@ -9,23 +11,29 @@ lambdaLocal.setLogger(winston)
 
 describe('helloWorld', function() {
   describe('with name supplied in event', function() {
-    var done, err;
+    var error, result
 
-    before(function(cb) {
+    before(function(done) {
+      sinon.stub(helloWorld, 'getGreetingPrefix').returns('Bonjour, ')
+
       lambdaLocal.execute({
-        event: {name: "Test"},
-        lambdaPath: './index.js',
-        callback: function(_err, _done) {
-          err = _err;
-          done = _done;
-          cb();
+        event       : {name: "Test"},
+        lambdaFunc  : helloWorld,
+        callback    : function(_error, _result) {
+          error = _error
+          result = _result
+          done()
         }
-      });
-    });
+      })
+    })
+
+    after(function() {
+      helloWorld.getGreetingPrefix.restore()
+    })
 
     it('should return custom greeting', function() {
-      assert.equal("Hello, Test", done.greeting);
-    });
-
-  });
-});
+      assert.equal("Bonjour, Test", result.greeting)
+      assert.equal(null, error)
+    })
+  })
+})
